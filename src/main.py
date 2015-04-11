4b6b8ea8f0a482190f79e6d68356490bf8bc9244
Alex Poon <chpoon92@gmail.com>
2015-04-12 02:28:42 +0800

    Implement bidding initialization

---
+++ src/main.py
@@ -0,0 +1,30 @@
+#!/usr/local/bin/python
+
+import re
+
+def initialize():
+    global bidders_num
+    global has_random_bidder
+    global rounds
+    global all_bids_are_announced
+    global are_discrete_bids
+    global bids
+    with open('setup.txt', 'r') as input:
+        bidders_num = input.readline().strip()
+        has_random_bidder = (input.readline().strip() == '1')
+        rounds = input.readline().strip()
+        all_bids_are_announced = (input.readline().strip() == '1')
+        bids_string = input.readline().strip()
+        are_discrete_bids = bool(re.compile('^\{.+\}$').match(bids_string))
+    bids = bids_string[1:len(bids_string)-1].split(',')
+    for i in range(len(bids)):
+        if re.compile('^\d+\/\d+$').match(bids[i]):
+            bids[i] = float(bids[i].split('/')[0]) / float(bids[i].split('/')[1])
+        bids[i] = float(bids[i])
+    bids.sort()
+       
+def main():
+    initialize()
+
+if __name__ == '__main__':
+    main()
