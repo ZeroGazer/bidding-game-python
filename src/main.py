ed1d8c4c79d4b069803a416e3c976665d10b6d84
Alex Poon <chpoon92@gmail.com>
2015-04-12 16:46:38 +0800

    Fix rounds counting bug

--- src/main.py
+++ src/main.py
@@ -6,15 +6,13 @@ def count_rounds():
     global current_round
     current_round = 1
     with open('result.txt', 'r') as input:
-        if not input.readline().strip():
-            return current_round
-        else:
-            while True:
-                for i in range(6):
-                    temp = input.readline()
-                current_round += 1
-                if not temp.strip():
-                    return current_round
+        input_str = input.readline().strip()
+        while input_str:
+            while not(input_str.startswith('end')):
+                input_str = input.readline().strip()
+            input_str = input.readline().strip()
+            input_str = input.readline().strip()
+            current_round += 1
 
 def initialize():
     global bidders_num
